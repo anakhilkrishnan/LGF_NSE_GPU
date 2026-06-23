8d3c7cca0af586096ee5341a10a4c4fb0f365ead
dragvangg <kakhil@iisc.ac.in>
2026-06-23 11:45:42 +0530

    Changes to job .sh script for GPU runs

--- Src/NSE_run/Utils/gpu_single.sh
+++ Src/NSE_run/Utils/gpu_single.sh
@@ -5,7 +5,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=2
 #SBATCH --gres=gpu:2
-#SBATCH --time=01:00:00
+#SBATCH --time=00:15:00
 #SBATCH --error=job.%J.err
 #SBATCH --output=job.%J.out
 
@@ -14,14 +14,14 @@ source ~/.bashrc
 
 # Environment Initialization 
 # (PARAM Pravega often utilizes 'spack load' alongside standard modules, adjust if required)
-module load gnu11/11.4.0
-module load openmpi/4.1.2
-module load cuda/12.9
+module load openmpi/openmpi_4.0.5_ucx_cuda_11.2_with_gcc
 
 # MPI and Networking Parameters
 export OMPI_MCA_btl_openib_allow_ib=1
 export OMPI_MCA_btl_openib_if_include="mlx5_0:1"
 export OMP_NUM_THREADS=1
+
+# export AMREX_USE_GPU_AWARE_MPI=1
 
 ulimit -s unlimited
 
